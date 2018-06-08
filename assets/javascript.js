9a9187b44094355a0c503d0e1dfe41e3cf814cba
Paraskevas Apostolopoulos <papostolopoulos@gmail.com>
2018-06-07 22:53:06 -0700

    worked on some js for portfolio

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,2 +1,71 @@
 "use strict"
 console.log("in the js file");
+
+let pages = {
+  writeIt: {
+    vanilla: "writeItVanilla",
+    jQuery: "writeItjQuery",
+    vue: "writeItVue",
+    react: "writeItReact",
+    angular: "writeItAngular"
+  },
+  listMe: {
+    vanilla: "listMeVanilla",
+    jQuery: "listMejQuery",
+    vue: "listMeVue",
+    react: "listMeReact",
+    angular: "listMeAngular"
+  },
+  hangman: {
+    vanilla: "hangmanVanilla",
+    jQuery: "hangmanjQuery",
+    vue: "hangmanVue",
+    react: "hangmanReact",
+    angular: "hangmanAngular"
+  },
+  sportyTourist: {
+    vanilla: "sportyTouristVanilla",
+    jQuery: "sportyTouristJQuery",
+    vue: "sportyTouristVue",
+    react: "sportyTouristReact",
+    angular: "sportyTouristAngular"
+  },
+  streetViewModal: {
+    vanilla: "streetViewModalVanilla",
+    jQuery: "streetViewModaljQuery",
+    vue: "streetViewModalVue",
+    react: "streetViewModalReact",
+    angular: "streetViewModalAngular"
+  },
+  drumKit: {
+    vanilla: "drumKitVanilla",
+    jQuery: "drumKitjQuery",
+    vue: "drumKitVue",
+    react: "drumKitReact",
+    angular: "drumKitAngular"
+  },
+  cssTutorial: {
+    vanilla: "cssTutorialVanilla",
+    jQuery: "cssTutorialjQuery",
+    vue: "cssTutorialVue",
+    react: "cssTutorialReact",
+    angular: "cssTutorialAngular"
+  },
+  spotifyDiscography: {
+    vanilla: "spotifyDiscographyVanilla",
+    jQuery: "spotifyDiscographyjQuery",
+    vue: "spotifyDiscographyVue",
+    react: "spotifyDiscographyReact",
+    angular: "spotifyDiscographyAngular"
+  },
+  imageFiltering: {
+    vanilla: "imageFilteringVanilla",
+    jQuery: "imageFilteringjQuery",
+    vue: "imageFilteringVue",
+    react: "imageFilteringReact",
+    angular: "imageFilteringAngular"
+  }
+}
+
+// document.querySelectorAll("a.screenshot")
+// Or is it onClick? How did you do it in the drum kit?
